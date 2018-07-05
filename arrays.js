c81563328c70520b792aa7d024a36bfb205bef9b
sunyounghwang <sunyounghwang13@gmail.com>
2018-07-05 18:58:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,10 +28,13 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
+  array.slice(1);
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array
+  array.pop();
+  return array;
 }
+
+
