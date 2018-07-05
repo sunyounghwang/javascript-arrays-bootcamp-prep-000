6291872b1ea2caf509c713a25b5f728e3f060489
sunyounghwang <sunyounghwang13@gmail.com>
2018-07-05 18:51:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, element]
+  [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
